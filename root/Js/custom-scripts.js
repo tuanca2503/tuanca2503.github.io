c197ccd377d60e499582b7f4fab2b121cf94fb08
TuanCARPSoft <tuan.ca.2503@gmail.com>
2025-05-30 13:34:43 +0700

    xx

--- root/Js/custom-scripts.js
+++ root/Js/custom-scripts.js
@@ -6,32 +6,32 @@
     }
   
       /* Loader Code Start */
-      $(window).on("load", function() { 
-          $(".section-loader").fadeOut("slow");
+      // $(window).on("load", function() { 
+      //     $(".section-loader").fadeOut("slow");
           
-          var $container = $('.portfolioContainer');
+      //     var $container = $('.portfolioContainer');
           
-          $container.isotope({
-              filter: '*',
-              animationOptions: {
-                  queue: true
-              }
-          });
+      //     $container.isotope({
+      //         filter: '*',
+      //         animationOptions: {
+      //             queue: true
+      //         }
+      //     });
        
-          $('.portfolio-nav li').click(function(){
-              $('.portfolio-nav .current').removeClass('current');
-              $(this).addClass('current');
+      //     $('.portfolio-nav li').click(function(){
+      //         $('.portfolio-nav .current').removeClass('current');
+      //         $(this).addClass('current');
        
-              var selector = $(this).attr('data-filter');
-              $container.isotope({
-                  filter: selector,
-                  animationOptions: {
-                      queue: true
-                  }
-               });
-               return false;
-          });
-        });
+      //         var selector = $(this).attr('data-filter');
+      //         $container.isotope({
+      //             filter: selector,
+      //             animationOptions: {
+      //                 queue: true
+      //             }
+      //          });
+      //          return false;
+      //     });
+      //   });
       /* Loader Code End */
 
 
